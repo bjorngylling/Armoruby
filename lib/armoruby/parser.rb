ea93c2283b15e31af49fc8c2281395c3ed9546df
Björn Gylling <bjorn.gylling@gmail.com>
2010-04-27 22:18:51 +0200

    Added talentspecs and made tests run faster due to only loading the character once.

--- lib/armoruby/parser.rb
+++ lib/armoruby/parser.rb
@@ -10,26 +10,38 @@ module Armoruby
       @realm = options[:realm] || "outland"
     end
     
-    def load_character(character_name)
+    def load_character(character_name)      
       url = "http://#{@region}.wowarmory.com/character-sheet.xml?r=#{@realm}&cn=#{character_name}"
       xml_doc = parse url
       
       character = xml_doc.at_css("character")
+      
+      talents = Array.new
+      xml_doc.css("talentSpec").each do |spec|
+        talents << {:name => spec['prim'],
+                    :tree => {:one => spec['treeOne'],
+                              :two => spec['treeTwo'],
+                              :three => spec['treeThree']},
+                    :active => spec.key?('active')}
+      end
+      
       result = {:name => character['name'],
                 :level => character['level'].to_i,
                 :class => {:id => character['classId'].to_i,
                            :name => character['class']},
                 :race => {:id => character['raceId'].to_i,
                           :name => character['race']},
-                :faction => character['faction']}
+                :faction => character['faction'],
+                :talents => talents}
       
       return result
     end
     
   private
     def parse(url)
+      options = {"User-Agent" => 'Mozilla/5.0 Gecko/20070219 Firefox/2.0.0.2'}
       
-      Nokogiri::XML(open(url, "User-Agent" => 'Mozilla/5.0 Gecko/20070219 Firefox/2.0.0.2'))
+      Nokogiri::XML(open(url, options))
       
     end
     
--- test/test_parser.rb
+++ test/test_parser.rb
@@ -3,77 +3,83 @@ require 'test/unit'
 require 'lib/armoruby'
 
 class DunderCharacterTest < Test::Unit::TestCase
+  
+  @@char = nil
 
-  def initialize(arg)
-    Armoruby.setup :region => "eu", :realm => "outland"
-    @char = Armoruby::Character.new("Tubal")
-    
-    super
+  def setup
+    if !@@char
+      Armoruby.setup :region => "eu", :realm => "outland"
+      @@char = Armoruby::Character.new("Tubal")
+    end
   end
 
   def test_class
-    assert_instance_of Armoruby::Character, @char
+    assert_instance_of Armoruby::Character, @@char
   end
   
   def test_name
-    assert_equal "Tubal", @char.name
+    assert_equal "Tubal", @@char.name
   end
   
   def test_level
-    assert_equal 80, @char.level
+    assert_equal 80, @@char.level
   end
   
   def test_klass
-    assert_equal "Priest", @char.klass[:name]
-    assert_equal 5, @char.klass[:id]
+    assert_equal "Priest", @@char.klass[:name]
+    assert_equal 5, @@char.klass[:id]
   end
   
   def test_faction
-    assert_equal "Alliance", @char.faction
+    assert_equal "Alliance", @@char.faction
   end
   
   def test_race
-    assert_equal "Dwarf", @char.race[:name]
-    assert_equal 3, @char.race[:id]
+    assert_equal "Dwarf", @@char.race[:name]
+    assert_equal 3, @@char.race[:id]
   end
   
-
+  def test_talents
+    assert_equal 2, @@char.talents.length
+  end
   
 end
 
 class DunderCharacterTestAnotherRegion < Test::Unit::TestCase
+  
+  @@char = nil
 
-  def initialize(arg)
-    Armoruby.setup :region => "us", :realm => "sen'jin"
-    @char = Armoruby::Character.new("Segolene")
-    
-    super
+  def setup
+    if !@@char
+      Armoruby.setup :region => "us", :realm => "sen'jin"
+      @@char = Armoruby::Character.new("Segolene")
+    end
   end
 
   def test_class
-    assert_instance_of Armoruby::Character, @char
+    assert_instance_of Armoruby::Character, @@char
   end
     
   def test_name
-    assert_equal "Segolene", @char.name
+    assert_equal "Segolene", @@char.name
   end
   
   def test_level
-    assert_equal 80, @char.level
+    assert_equal 80, @@char.level
   end
   
   def test_klass
-    assert_equal "Priest", @char.klass[:name]
-    assert_equal 5, @char.klass[:id]
+    assert_equal "Priest", @@char.klass[:name]
+    assert_equal 5, @@char.klass[:id]
   end
   
   def test_faction
-    assert_equal "Alliance", @char.faction
+    assert_equal "Alliance", @@char.faction
   end
   
   def test_race
-    assert_equal "Night Elf", @char.race[:name]
-    assert_equal 4, @char.race[:id]
+    assert_equal "Night Elf", @@char.race[:name]
+    assert_equal 4, @@char.race[:id]
   end
   
 end
